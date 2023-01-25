45721922e9e25774374032e2ee259e97b7381dd7
Yabo Lindile <yabo.lindile@younglings.africa>
2023-01-25 20:04:23 +0200

    second commit

---
+++ models/convoModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const convoSchhema = mongoose.Schema({
+  _id: {
+    type: mongoose.Types.ObjectId,
+  },
+  youthName: {
+    type: String,
+    required: [true, "youth name is undefined"],
+  },
+  agentName: {
+    type: String,
+    required: [true, "agent name is undefined"],
+  },
+  psychosocialExchange: {
+    type: Object,
+    required: [true, "agent name is undefined"],
+  },
+  callType: {
+    type: String,
+    min: 4,
+    max: 15,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+    required: [true, "agent name is undefined"],
+  },
+});
